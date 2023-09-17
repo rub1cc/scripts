8649dcd655de1a4d81c381a3a03495e4fba2bd9a
Faraz Aulia <hey@rub1.cc>
2023-09-17 12:45:12 +0700

    Create setup.sh

---
+++ code-server/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# install packages
+sudo apt-get update 
+sudo apt-get install -y -q --no-install-recommends curl git wget vim
+sudo rm -rf /var/lib/apt/lists/*
+
+
+# install nvm if not exist
+if [ ! -d ~/.nvm ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+fi
+
+# install node 16.6.1
+. ~/.nvm/nvm.sh
+nvm install --lts
+nvm install 16.6.1
+
+# create .vscode folder in home if not exist
+if [ ! -d ~/.vscode ]; then
+  mkdir ~/.vscode
+fi
+if [ ! -d ~/.vscode/extensions ]; then
+  mkdir ~/.vscode/extensions
+fi
+
+# download vscode extensions list
+echo "Downloading vscode settings and extensions list..."
+curl https://raw.githubusercontent.com/rub1cc/.dotfiles/master/.vscode/settings.json > ~/data/User/settings.json
+curl https://raw.githubusercontent.com/rub1cc/.dotfiles/master/.vscode/extensions.txt -o ~/.vscode/extensions.txt
+
+# install vscode extensions
+echo "Installing vscode extensions..."
+cd ~/.vscode
+curl https://raw.githubusercontent.com/rub1cc/scripts/main/code-server/download-vsix.sh | bash
+curl https://raw.githubusercontent.com/rub1cc/scripts/main/code-server/install-vsix.sh | bash
